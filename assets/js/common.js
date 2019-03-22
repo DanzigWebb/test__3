e93b45f1eb0b5913ea0b621bdb23cb8c890d1634
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-03-22 07:03:27 +0300

    commit

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,23 +1,54 @@
+$(document).ready(function() {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
 
- $(document).ready(function() {
-            $("#my-menu").mmenu({
-                extensions: ['effect-menu-slide', 'pagedim-black', "fx-listitems-fade", 'position-right'],
-                navbar: {
-                    //title: '<img src="/assets/img/main/logo-1.png">'
-                },
-            }, {
-                // configuration
-                offCanvas: {
-                    pageSelector: "#my-page"
-                }
-            });
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+    });
+    $('.popup-with-commits').magnificPopup({
+		type: 'inline',
+		preloader: false,
 
-            var api = $('#my-menu').data('mmenu');
-            api.bind('open:start', function() {
-                $('.hamburger').addClass('is-active');
-            });
-            api.bind('close:before', function() {
-                $('.hamburger').removeClass('is-active');
-            })
-        });
+	});
+});
 
+$(document).ready(function(){
+    $('.comments-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsiveClass:false,
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+                dots:false,
+                loop:true
+            },
+            600:{
+                items:1,
+                dots:false,
+                nav:true
+            },
+            1000:{
+                items:1,
+                nav:true,
+                dots:false,
+                loop:true
+            }
+        }
+    });
+})
